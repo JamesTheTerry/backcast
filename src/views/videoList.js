f1fa94c60d35ea3fba13f83f6fd31fede467868f
Hack Reactor Student <student@hackreactor.com>
2017-10-23 16:33:16 -0700

    Gets all the way to video list entries

--- src/views/videoList.js
+++ src/views/videoList.js
@@ -1,12 +1,21 @@
 var VideoListView = Backbone.View.extend({
 
-
   render: function() {
     this.$el.children().detach();
     this.$el.html(this.template());
+    this.$el.children().children().detach(); // we're clearing the children in a very weird way
+    this.$el.children().append(
+      this.collection.map(this.renderVideoListEntry));
     return this;
   },
 
-  template: templateURL('src/templates/videoList.html')
+  template: templateURL('src/templates/videoList.html'),
+
+  renderVideoListEntry: function(video) {
+    // var videoListEntry = new VideoListEntryView({model: video});
+    // // console.log(videoListEntry.render);
+    // this.$el.children().append(videoListEntry.render());
+    return new VideoListEntryView({model: video}).render().$el;
+  }
 
 });
