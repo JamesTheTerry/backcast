3c6aa24f082c2a6e94767c74d3995540d4c6230d
Hack Reactor Student <student@hackreactor.com>
2017-10-23 19:51:17 -0700

    began the dark desent into youtube search

--- src/collections/videos.js
+++ src/collections/videos.js
@@ -2,7 +2,13 @@ var Videos = Backbone.Collection.extend({
 
   model: Video,
 
-  initialize: function() {},
+  initialize: function() {
+    this.url = 'https://www.googleapis.com/youtube/v3/search';
+  },
 
+  // learn how to search on Tuesday
+  search: function() {
+    // fetch()
+  }
 
 });
