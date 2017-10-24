2ada11eb2982e062d83889c6a46f8db60972b0da
Hack Reactor Student <student@hackreactor.com>
2017-10-23 20:07:49 -0700

    sans explns

--- src/collections/videos.js
+++ src/collections/videos.js
@@ -7,8 +7,21 @@ var Videos = Backbone.Collection.extend({
   },
 
   // learn how to search on Tuesday
-  search: function() {
-    // fetch()
+  search: function(query) {
+    this.fetch({
+      data: {
+        'key': window.YOUTUBE_API_KEY,
+        'maxResults': '5',
+        'part': 'snippet',
+        'q' : query,
+        'type': 'video',
+        'videoEmbeddable' : 'true'
+      }
+    });
+  },
+
+  parse: function(response) {
+    console.log('response', response);
   }
 
 });
