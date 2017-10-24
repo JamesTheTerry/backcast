04209dfc55c1d6985d6e73e38a02bb2bfb4945b3
Hack Reactor Student <student@hackreactor.com>
2017-10-23 19:09:13 -0700

    Creates a Dynamic Video Player We give the VideoPlayerView access to the video collection, which it listens to. If a single video title is clicked, select in the model binds it to the particular video and then calls a function that switches the model video to this new one and re renders.

--- src/views/videoPlayer.js
+++ src/views/videoPlayer.js
@@ -1,5 +1,8 @@
 var VideoPlayerView = Backbone.View.extend({
 
+  initialize: function() {
+    this.listenTo(this.collection, 'select', this.switchVideo);
+  },
 
   render: function() {
     this.$el.html('<div class="loading">Please wait...</div>'); // why is this here?
@@ -7,6 +10,12 @@ var VideoPlayerView = Backbone.View.extend({
     console.log('curr', this);
     this.$el.html(this.template(this.model.attributes));
     return this;
+  },
+
+  switchVideo: function(clickedVideo) {
+    console.log('clickedVideo', clickedVideo);
+    this.model = clickedVideo;
+    this.render();
   },
 
   template: templateURL('src/templates/videoPlayer.html')
